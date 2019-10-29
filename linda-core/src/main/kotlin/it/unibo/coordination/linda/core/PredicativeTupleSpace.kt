daaa0b7653c2737ceeee75df1f2b1bf8d944ddfb
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2019-10-29 22:14:41 +0100

    parsing methods in PredicativeTupleSpace

--- linda-core/src/main/kotlin/it/unibo/coordination/linda/core/PredicativeTupleSpace.kt
+++ linda-core/src/main/kotlin/it/unibo/coordination/linda/core/PredicativeTupleSpace.kt
@@ -7,14 +7,30 @@ interface PredicativeTupleSpace<T : Tuple, TT : Template, K, V> : TupleSpace<T, 
     fun tryTake(template: TT): Promise<Match<T, TT, K, V>>
 
     @JvmDefault
+    fun tryTake(template: String): Promise<Match<T, TT, K, V>> =
+            tryTake(template.toTemplate())
+
+    @JvmDefault
     fun tryTakeTuple(template: TT): Promise<Optional<T>> {
         return tryTake(template).thenApplyAsync{ it.tuple }
     }
 
+    @JvmDefault
+    fun tryTakeTuple(template: String): Promise<Optional<T>> =
+            tryTakeTuple(template.toTemplate())
+
     fun tryRead(template: TT): Promise<Match<T, TT, K, V>>
+
+    @JvmDefault
+    fun tryRead(template: String): Promise<Match<T, TT, K, V>> =
+            tryRead(template.toTemplate())
 
     @JvmDefault
     fun tryReadTuple(template: TT): Promise<Optional<T>> {
         return tryRead(template).thenApplyAsync{ it.tuple }
     }
+
+    @JvmDefault
+    fun tryReadTuple(template: String): Promise<Optional<T>> =
+            tryReadTuple(template.toTemplate())
 }
