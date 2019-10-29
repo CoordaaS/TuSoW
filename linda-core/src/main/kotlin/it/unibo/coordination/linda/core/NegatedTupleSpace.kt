745cc84c13f382215e3499c776889565e433f036
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2019-10-29 22:18:39 +0100

    parsing methods in NegatedTupleSpace

--- linda-core/src/main/kotlin/it/unibo/coordination/linda/core/NegatedTupleSpace.kt
+++ linda-core/src/main/kotlin/it/unibo/coordination/linda/core/NegatedTupleSpace.kt
@@ -8,14 +8,30 @@ interface NegatedTupleSpace<T : Tuple, TT : Template, K, V> : TupleSpace<T, TT, 
     fun absent(template: TT): Promise<Match<T, TT, K, V>>
 
     @JvmDefault
+    fun absent(template: String): Promise<Match<T, TT, K, V>> =
+            absent(template.toTemplate())
+
+    @JvmDefault
     fun absentTemplate(template: TT): Promise<TT> {
         return absent(template).thenApplyAsync { it.template }
     }
 
+    @JvmDefault
+    fun absentTemplate(template: String): Promise<TT> =
+            absentTemplate(template.toTemplate())
+
     fun tryAbsent(template: TT): Promise<Match<T, TT, K, V>>
+
+    @JvmDefault
+    fun tryAbsent(template: String): Promise<Match<T, TT, K, V>> =
+            tryAbsent(template.toTemplate())
 
     @JvmDefault
     fun tryAbsentTuple(template: TT): Promise<Optional<T>> {
         return tryAbsent(template).thenApplyAsync { it.tuple }
     }
+
+    @JvmDefault
+    fun tryAbsentTuple(template: String): Promise<Optional<T>> =
+            tryAbsentTuple(template.toTemplate())
 }
