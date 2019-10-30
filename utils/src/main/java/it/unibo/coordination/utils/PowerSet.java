d0d8a7dadf16093851b47787fa06710e8c81e6c3
Giovanni <giovanni.ciatto@unibo.it>
2019-10-30 09:48:42 +0100

    format PowerSet

--- utils/src/main/java/it/unibo/coordination/utils/PowerSet.java
+++ utils/src/main/java/it/unibo/coordination/utils/PowerSet.java
@@ -1,22 +1,22 @@
 package it.unibo.coordination.utils;
 
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 public class PowerSet {
-    public static <T> Set<Set<T>> powerSet( Set<T> set ) {
+    public static <T> Set<Set<T>> powerSet(Set<T> set) {
         T[] element = (T[]) set.toArray();
         final int SET_LENGTH = 1 << element.length;
         Set<Set<T>> powerSet = new HashSet<>();
-        for( int binarySet = 0; binarySet < SET_LENGTH; binarySet++ ) {
+        for (int binarySet = 0; binarySet < SET_LENGTH; binarySet++) {
             Set<T> subset = new HashSet<>();
-            for( int bit = 0; bit < element.length; bit++ ) {
+            for (int bit = 0; bit < element.length; bit++) {
                 int mask = 1 << bit;
-                if( (binarySet & mask) != 0 ) {
-                    subset.add( element[bit] );
+                if ((binarySet & mask) != 0) {
+                    subset.add(element[bit]);
                 }
             }
-            powerSet.add( subset );
+            powerSet.add(subset);
         }
         return powerSet;
     }
