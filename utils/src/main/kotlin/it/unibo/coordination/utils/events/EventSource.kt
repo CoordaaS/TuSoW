203b41baeb56687b4e4c32a76a80fd180c6784ae
Giovanni <giovanni.ciatto@unibo.it>
2019-10-30 09:55:01 +0100

    operators for event event source

--- utils/src/main/kotlin/it/unibo/coordination/utils/events/EventSource.kt
+++ utils/src/main/kotlin/it/unibo/coordination/utils/events/EventSource.kt
@@ -3,7 +3,15 @@ package it.unibo.coordination.utils.events
 interface EventSource<Arg> {
     fun bind(listener: EventListener<Arg>)
 
+    @JvmDefault
+    operator fun plusAssign(listener: EventListener<Arg>) =
+            bind(listener)
+
     fun unbind(listener: EventListener<Arg>)
+
+    @JvmDefault
+    operator fun minusAssign(listener: EventListener<Arg>) =
+            unbind(listener)
 
     fun unbindAll()
 }
