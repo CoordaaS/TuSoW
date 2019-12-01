69027a0bc23064040f7e01f02645a2228f6f5b87
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2019-12-01 20:43:51 +0100

    explicit factory methods in Remote Spaces

--- linda-logic-client/src/main/kotlin/it/unibo/coordination/linda/logic/remote/RemoteLogicSpace.kt
+++ linda-logic-client/src/main/kotlin/it/unibo/coordination/linda/logic/remote/RemoteLogicSpace.kt
@@ -11,25 +11,56 @@ import java.net.URL
 interface RemoteLogicSpace : LogicSpace, RemoteTupleSpace<LogicTuple, LogicTemplate, String, Term, LogicMatch> {
     companion object {
         @JvmStatic
-        @JvmOverloads
-        fun of(address: String = "localhost", port: Int = 8080, name: String = "default"): RemoteLogicSpace {
+        fun of(address: String, port: Int, name: String): RemoteLogicSpace {
             return of(URL("http", address, port, ""), name)
         }
 
         @JvmStatic
-        @JvmOverloads
-        fun of(url: URL, name: String = "default"): RemoteLogicSpace {
+        fun of(address: String, name: String): RemoteLogicSpace {
+            return of(address, 8080, name)
+        }
+
+        @JvmStatic
+        fun of(name: String): RemoteLogicSpace {
+            return of("localhost", name)
+        }
+
+        @JvmStatic
+        fun of(): RemoteLogicSpace {
+            return of("default")
+        }
+
+        @JvmStatic
+        fun of(port: Int, name: String): RemoteLogicSpace {
+            return of("localhost", port, name)
+        }
+
+        @JvmStatic
+        fun of(port: Int): RemoteLogicSpace {
+            return of(port, "default")
+        }
+
+        @JvmStatic
+        fun of(address: String, port: Int): RemoteLogicSpace {
+            return of(address, port, "default")
+        }
+
+        @JvmStatic
+        fun of(url: URL, name: String): RemoteLogicSpace {
             return RemoteLogicSpaceImpl(url, name)
+        }
+
+        @JvmStatic
+        fun of(url: URL): RemoteLogicSpace {
+            return of(url, "default")
         }
     }
 }
 
-@JvmOverloads
 fun LogicSpace.remote(address: String = "localhost", port: Int = 8080, name: String = "default"): RemoteLogicSpace {
     return RemoteLogicSpace.of(address, port, name)
 }
 
-@JvmOverloads
 fun LogicSpace.remote(url: URL, name: String = "default"): RemoteLogicSpace {
     return RemoteLogicSpace.of(url, name)
 }
