e8ad90c2bfa40cd56db6e11a2a4a1aa712c7bf08
Giovanni <giovanni.ciatto@unibo.it>
2020-01-31 08:52:59 +0100

    + controller

--- control/src/main/kotlin/it/unibo/coordination/control/Activity.kt
+++ control/src/main/kotlin/it/unibo/coordination/control/Activity.kt
@@ -1,6 +1,14 @@
 package it.unibo.coordination.control
 
 interface Activity {
+
+    interface Controller {
+        fun stop();
+        fun restart();
+        fun pause();
+        fun `continue`();
+    }
+
     fun onBegin()
     fun onStep()
     fun onEnd()
