057959b248e60c9790b734db878aba88a2a006cf
Filippo Cavallari <filippo.cavallari99@gmail.com>
2022-02-23 18:31:02 +0100

    Removed unused imports

--- tusow-grpc-text/src/test/kotlin/TextClientTest.kt
+++ tusow-grpc-text/src/test/kotlin/TextClientTest.kt
@@ -8,7 +8,8 @@ import io.grpc.stub.StreamObserver
 import it.unibo.coordaas.tusow.grpc.text.TextualGRPCHandler
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import java.util.*
