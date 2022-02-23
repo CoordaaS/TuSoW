f90fec4172821bb19a57ae0f8ab822bcc50ecd91
Filippo Cavallari <filippo.cavallari99@gmail.com>
2022-02-23 17:41:26 +0100

    Refactored code to remove duplicated lines

--- tusow-grpc-text/src/main/kotlin/it/unibo/coordaas/tusow/grpc/text/TextualGRPCHandler.kt
+++ tusow-grpc-text/src/main/kotlin/it/unibo/coordaas/tusow/grpc/text/TextualGRPCHandler.kt
@@ -2,6 +2,7 @@ package it.unibo.coordaas.tusow.grpc.text
 
 import io.grpc.stub.StreamObserver
 import it.unibo.coordination.Promise
+import it.unibo.coordination.linda.logic.LogicMatch
 import it.unibo.coordination.linda.text.RegexTemplate
 import it.unibo.coordination.linda.text.RegularMatch
 import it.unibo.coordination.linda.text.StringTuple
@@ -11,39 +12,97 @@ import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import TusowGRPC.*
 
 class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
 
     private val textualSpaces: MutableMap<String, TextualSpace> = HashMap()
     private var executor: ExecutorService = Executors.newSingleThreadExecutor()
 
+    private fun setPromiseTimeout(promise: Promise<RegularMatch>, request: ReadOrTakeRequest){
+        promise.completeOnTimeout(
+            RegularMatch.failed(RegexTemplate.of(request.textualTemplate.regex)),
+            5000,
+            TimeUnit.MILLISECONDS
+        )
+    }
+
+    private fun handleReadOrTakePromise(promise: Promise<RegularMatch>, request: ReadOrTakeRequest, responseObserver: StreamObserver<Tuple>){
+        setPromiseTimeout(promise, request)
+        promise.thenAccept {
+            var value = ""
+            it.tuple.ifPresent { logicTuple ->
+                value = logicTuple.value.toString()
+            }
+            responseObserver.onNext(Tuple.newBuilder().setValue(value).build())
+            responseObserver.onCompleted()
+        }
+    }
+
+    private fun handleReadOrTakeAllPromise(completableFutures: ArrayList<Promise<RegularMatch>>, responseObserver: StreamObserver<TuplesList>){
+        val futuresArray: Array<Promise<RegularMatch>> = completableFutures.toTypedArray()
+        val finalFuture = CompletableFuture.allOf(*futuresArray)
+        val tuplesList = TuplesList.newBuilder()
+        finalFuture.thenApply {
+            completableFutures.forEach {
+                val regularMatch = it.join()
+                val tuple = Tuple.newBuilder().setValue(regularMatch.tuple.get().value).build()
+                tuplesList.addTuples(tuple)
+            }
+            responseObserver.onNext(tuplesList.build())
+            responseObserver.onCompleted()
+        }
+    }
+
+    private fun getWritePromisesList(request: WriteAllRequest, space: TextualSpace) : ArrayList<Promise<StringTuple>>{
+        val completableFutures = ArrayList<Promise<StringTuple>>()
+        request.tuplesList.tuplesList.forEach { tuple ->
+            val promise = space.write(tuple.value)
+            completableFutures.add(promise)
+        }
+        return completableFutures
+    }
+
+    private fun handleReadOrTakeAllAsStreamPromise(completableFutures: ArrayList<Promise<RegularMatch>>, responseObserver: StreamObserver<Tuple>){
+        val futuresArray: Array<Promise<RegularMatch>> = completableFutures.toTypedArray()
+        val finalFuture = CompletableFuture.allOf(*futuresArray)
+        finalFuture.thenApply {
+            completableFutures.forEach {
+                val regularMatch = it.join()
+                val tuple = Tuple.newBuilder().setValue(regularMatch.tuple.get().value).build()
+                responseObserver.onNext(tuple)
+            }
+            responseObserver.onCompleted()
+        }
+    }
+
     override fun validateTupleSpace(
-        request: TusowGRPC.TupleSpace,
-        responseObserver: StreamObserver<TusowGRPC.IOResponse>
+        request: TupleSpace,
+        responseObserver: StreamObserver<IOResponse>
     ) {
         responseObserver.onNext(
-            TusowGRPC.IOResponse.newBuilder().setResponse(textualSpaces.containsKey(request.name))
+            IOResponse.newBuilder().setResponse(textualSpaces.containsKey(request.name))
                 .setMessage(textualSpaces[request.name]?.name ?: "NULL").build()
         )
         responseObserver.onCompleted()
     }
 
     override fun createTupleSpace(
-        request: TusowGRPC.TupleSpace,
-        responseObserver: StreamObserver<TusowGRPC.IOResponse>
+        request: TupleSpace,
+        responseObserver: StreamObserver<IOResponse>
     ) {
         textualSpaces[request.name] = TextualSpace.local(request.name, executor)
         responseObserver.onNext(
-            TusowGRPC.IOResponse.newBuilder().setResponse(true).setMessage(request.name).build()
+            IOResponse.newBuilder().setResponse(true).setMessage(request.name).build()
         )
         responseObserver.onCompleted()
     }
 
-    override fun write(request: TusowGRPC.WriteRequest, responseObserver: StreamObserver<TusowGRPC.IOResponse>) {
+    override fun write(request: WriteRequest, responseObserver: StreamObserver<IOResponse>) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
             responseObserver.onNext(
-                TusowGRPC.IOResponse.newBuilder().setResponse(false)
+                IOResponse.newBuilder().setResponse(false)
                     .setMessage("{error: \"Invalid tuple space name\"}").build()
             )
             responseObserver.onCompleted()
@@ -51,70 +110,54 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
             val promise = space.write(request.tuple.value)
             promise.thenAccept {
                 responseObserver.onNext(
-                    TusowGRPC.IOResponse.newBuilder().setResponse(true).setMessage(it.value).build()
+                    IOResponse.newBuilder().setResponse(true).setMessage(it.value).build()
                 )
                 responseObserver.onCompleted()
             }
         }
     }
 
-    override fun read(request: TusowGRPC.ReadOrTakeRequest, responseObserver: StreamObserver<TusowGRPC.Tuple>) {
+    override fun read(request: ReadOrTakeRequest, responseObserver: StreamObserver<Tuple>) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
             responseObserver.onCompleted()
         } else {
             val promise = space.read(request.textualTemplate.regex)
-            //TODO Does TupleSpace.read() timeout by himself or do I need to call CompletableFuture<M>.completeOnTimeout()?
-            promise.completeOnTimeout(
-                RegularMatch.failed(RegexTemplate.of(request.textualTemplate.regex)),
-                5000,
-                TimeUnit.MILLISECONDS
-            )
-            promise.thenAccept {
-                responseObserver.onNext(TusowGRPC.Tuple.newBuilder().setValue(it.tuple.get().value).build())
-                responseObserver.onCompleted()
-            }
+            handleReadOrTakePromise(promise, request, responseObserver)
         }
     }
 
-    override fun take(request: TusowGRPC.ReadOrTakeRequest, responseObserver: StreamObserver<TusowGRPC.Tuple>) {
+    override fun take(request: ReadOrTakeRequest, responseObserver: StreamObserver<Tuple>) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
-            responseObserver.onNext(TusowGRPC.Tuple.newBuilder().setValue("{tuple: null}").build())
+            responseObserver.onNext(Tuple.newBuilder().setValue("{tuple: null}").build())
             responseObserver.onCompleted()
         } else {
             val promise = space.take(request.textualTemplate.regex)
-            promise.thenAccept {
-                responseObserver.onNext(TusowGRPC.Tuple.newBuilder().setValue(it.tuple.get().value).build())
-                responseObserver.onCompleted()
-            }
+            handleReadOrTakePromise(promise, request, responseObserver)
         }
     }
 
     override fun writeAll(
-        request: TusowGRPC.WriteAllRequest,
-        responseObserver: StreamObserver<TusowGRPC.IOResponseList>
+        request: WriteAllRequest,
+        responseObserver: StreamObserver<IOResponseList>
     ) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
-            val response = TusowGRPC.IOResponse.newBuilder().setResponse(false)
+            val response = IOResponse.newBuilder().setResponse(false)
                 .setMessage("{error: \"Invalid tuple space name\"}").build()
-            responseObserver.onNext(TusowGRPC.IOResponseList.newBuilder().addResponses(0, response).build())
+            responseObserver.onNext(IOResponseList.newBuilder().addResponses(0, response).build())
             responseObserver.onCompleted()
         } else {
-            val completableFutures = ArrayList<Promise<StringTuple>>()
-            request.tuplesList.tuplesList.forEach { tuple ->
-                val promise = space.write(tuple.value)
-                completableFutures.add(promise)
-            }
+            val completableFutures = getWritePromisesList(request, space)
             val futuresArray: Array<Promise<StringTuple>> = completableFutures.toTypedArray()
             val finalFuture = CompletableFuture.allOf(*futuresArray)
-            val ioResponseListBuilder = TusowGRPC.IOResponseList.newBuilder()
+            val ioResponseListBuilder = IOResponseList.newBuilder()
             finalFuture.thenApply {
                 completableFutures.forEach {
                     val stringTuple = it.join()
                     val ioResponse =
-                        TusowGRPC.IOResponse.newBuilder().setResponse(stringTuple.value.isNotEmpty())
+                        IOResponse.newBuilder().setResponse(stringTuple.value.isNotEmpty())
                             .setMessage(stringTuple.value).build()
                     ioResponseListBuilder.addResponses(ioResponse)
                 }
@@ -125,8 +168,8 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
     }
 
     override fun readAll(
-        request: TusowGRPC.ReadOrTakeAllRequest,
-        responseObserver: StreamObserver<TusowGRPC.TuplesList>
+        request: ReadOrTakeAllRequest,
+        responseObserver: StreamObserver<TuplesList>
     ) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
@@ -137,24 +180,13 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
                 val promise = space.read(regex.regex)
                 completableFutures.add(promise)
             }
-            val futuresArray: Array<Promise<RegularMatch>> = completableFutures.toTypedArray()
-            val finalFuture = CompletableFuture.allOf(*futuresArray)
-            val tuplesList = TusowGRPC.TuplesList.newBuilder()
-            finalFuture.thenApply {
-                completableFutures.forEach {
-                    val regularMatch = it.join()
-                    val tuple = TusowGRPC.Tuple.newBuilder().setValue(regularMatch.tuple.get().value).build()
-                    tuplesList.addTuples(tuple)
-                }
-                responseObserver.onNext(tuplesList.build())
-                responseObserver.onCompleted()
-            }
+            handleReadOrTakeAllPromise(completableFutures, responseObserver)
         }
     }
 
     override fun takeAll(
-        request: TusowGRPC.ReadOrTakeAllRequest,
-        responseObserver: StreamObserver<TusowGRPC.TuplesList>
+        request: ReadOrTakeAllRequest,
+        responseObserver: StreamObserver<TuplesList>
     ) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
@@ -165,44 +197,29 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
                 val promise = space.take(regex.regex)
                 completableFutures.add(promise)
             }
-            val futuresArray: Array<Promise<RegularMatch>> = completableFutures.toTypedArray()
-            val finalFuture = CompletableFuture.allOf(*futuresArray)
-            val tuplesList = TusowGRPC.TuplesList.newBuilder()
-            finalFuture.thenApply {
-                completableFutures.forEach {
-                    val regularMatch = it.join()
-                    val tuple = TusowGRPC.Tuple.newBuilder().setValue(regularMatch.tuple.get().value).build()
-                    tuplesList.addTuples(tuple)
-                }
-                responseObserver.onNext(tuplesList.build())
-                responseObserver.onCompleted()
-            }
+            handleReadOrTakeAllPromise(completableFutures, responseObserver)
         }
     }
 
     override fun writeAllAsStream(
-        request: TusowGRPC.WriteAllRequest,
-        responseObserver: StreamObserver<TusowGRPC.IOResponse>
+        request: WriteAllRequest,
+        responseObserver: StreamObserver<IOResponse>
     ) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
-            val response = TusowGRPC.IOResponse.newBuilder().setResponse(false)
+            val response = IOResponse.newBuilder().setResponse(false)
                 .setMessage("{error: \"Invalid tuple space name\"}").build()
             responseObserver.onNext(response)
             responseObserver.onCompleted()
         } else {
-            val completableFutures = ArrayList<Promise<StringTuple>>()
-            request.tuplesList.tuplesList.forEach { tuple ->
-                val promise = space.write(tuple.value)
-                completableFutures.add(promise)
-            }
+            val completableFutures = getWritePromisesList(request, space)
             val futuresArray: Array<Promise<StringTuple>> = completableFutures.toTypedArray()
             val finalFuture = CompletableFuture.allOf(*futuresArray)
             finalFuture.thenApply {
                 completableFutures.forEach {
                     val stringTuple = it.join()
                     val ioResponse =
-                        TusowGRPC.IOResponse.newBuilder().setResponse(stringTuple.value.isNotEmpty())
+                        IOResponse.newBuilder().setResponse(stringTuple.value.isNotEmpty())
                             .setMessage(stringTuple.value).build()
                     responseObserver.onNext(ioResponse)
                 }
@@ -212,8 +229,8 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
     }
 
     override fun readAllAsStream(
-        request: TusowGRPC.ReadOrTakeAllRequest,
-        responseObserver: StreamObserver<TusowGRPC.Tuple>
+        request: ReadOrTakeAllRequest,
+        responseObserver: StreamObserver<Tuple>
     ) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
@@ -225,22 +242,13 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
                 val promise = space.read(regex.regex)
                 completableFutures.add(promise)
             }
-            val futuresArray: Array<Promise<RegularMatch>> = completableFutures.toTypedArray()
-            val finalFuture = CompletableFuture.allOf(*futuresArray)
-            finalFuture.thenApply {
-                completableFutures.forEach {
-                    val regularMatch = it.join()
-                    val tuple = TusowGRPC.Tuple.newBuilder().setValue(regularMatch.tuple.get().value).build()
-                    responseObserver.onNext(tuple)
-                }
-                responseObserver.onCompleted()
-            }
+            handleReadOrTakeAllAsStreamPromise(completableFutures, responseObserver)
         }
     }
 
     override fun takeAllAsStream(
-        request: TusowGRPC.ReadOrTakeAllRequest,
-        responseObserver: StreamObserver<TusowGRPC.Tuple>
+        request: ReadOrTakeAllRequest,
+        responseObserver: StreamObserver<Tuple>
     ) {
         val space = textualSpaces[request.tupleSpace.name]
         if (space == null) {
@@ -251,16 +259,7 @@ class TextualGRPCHandler : TusowServiceGrpc.TusowServiceImplBase(){
                 val promise = space.take(regex.regex)
                 completableFutures.add(promise)
             }
-            val futuresArray: Array<Promise<RegularMatch>> = completableFutures.toTypedArray()
-            val finalFuture = CompletableFuture.allOf(*futuresArray)
-            finalFuture.thenApply {
-                completableFutures.forEach {
-                    val regularMatch = it.join()
-                    val tuple = TusowGRPC.Tuple.newBuilder().setValue(regularMatch.tuple.get().value).build()
-                    responseObserver.onNext(tuple)
-                }
-                responseObserver.onCompleted()
-            }
+            handleReadOrTakeAllAsStreamPromise(completableFutures, responseObserver)
         }
     }
 }
